851c4a3debeae4b65b8d3a9d3baca988382bd228
anys34 <anyeseong34@gmail.com>
2023-08-21 02:32:53 +0900

    ADD :: github oauth

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 
+const pageRouter = require("./routes/page")
+const authRouter = require("./routes/auth")
+
 const app = express();
 app.set("port", process.env.PORT || 3000);
+
+app.use("/", pageRouter);
+app.use("/auth", authRouter);
 
 app.listen(app.get("port"), () => {
     console.log(app.get("port"), "node start!");
---
+++ routes/auth.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
+
+const env = process.env;
+
+router.get("/", (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${ env.CLIENT_ID }`
+  );
+});
+
+router.get("/github/callback", (req, res) => {
+  axios
+    .post(
+      "https://github.com/login/oauth/access_token",
+      {
+        client_id: env.CLIENT_ID,
+        client_secret: env.CLIENT_SECRET,
+        code: req.query.code,
+      },
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    )
+    .then((result) => {
+      console.log(result.data.access_token);
+      res.send("you are authorized " + result.data.access_token);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
