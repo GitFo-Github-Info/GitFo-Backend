d7acbe109029e6f5c1cf9321e4b89eff7bb5a733
anys34 <anyeseong34@gmail.com>
2023-08-22 22:51:15 +0900

    ADD :: search users

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+require("dotenv").config();
+
+const env = process.env;
 
 router.get("/:id", (req, res) => {
   const id = req.params.id;
@@ -8,7 +11,7 @@ router.get("/:id", (req, res) => {
     .get(`https://api.github.com/users/${id}`)
     .then((response) => {
       const user_data = response.data;
-    //   console.log(user_data);
+      //   console.log(user_data);
       res.send(user_data);
     })
     .catch((error) => {
@@ -17,14 +20,51 @@ router.get("/:id", (req, res) => {
     });
 });
 
-router.get("/users/:id", (req, res) => {
-  const id = req.params.id;
+router.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const page = req.query.page;
+  let users_Info = [];
   axios
-    .get(`https://api.github.com/search/users?q=${id}`)
-    .then((response) => {
+    .get(
+      `https://api.github.com/search/users?q=${id}&per_page=10&page=${page}`,
+      {
+        headers: {
+          Authorization: "Bearer " + env.GITHUB_TOKEN,
+        },
+      }
+    )
+    .then(async (response) => {
       const users = response.data;
-    //   console.log(users);
-      res.send(users);
+      const requests = users.items.map((item) => {
+        if (item === undefined) {
+          return null;
+        }
+        return axios.get(item.url, {
+          headers: {
+            Authorization: "Bearer " + env.GITHUB_TOKEN,
+          },
+        });
+      });
+
+      const allResponses = await Promise.all(requests);
+      allResponses.forEach((user_Info, index) => {
+        if (user_Info) {
+          const user = user_Info.data;
+          users_Info.push({
+            profile_img: user.avatar_url,
+            id: user.login,
+            name: user.name,
+            bio: user.bio,
+            company: user.company,
+            public_repos: user.public_repos,
+            followers: user.followers,
+            following: user.following,
+            location: user.location,
+          });
+        }
+      });
+      users_Info.push(users.total_count);
+      res.send(users_Info);
     })
     .catch((error) => {
       console.log("API 호출 실패:", error.response.status);
